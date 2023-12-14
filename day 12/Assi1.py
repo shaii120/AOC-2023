844e6f6868bf2de211d3f697517a58a626ec6452
Shai Komisar <kofmisar@gmail.com>
2023-12-14 22:33:45 +0200

    day 12

--- day 12/Assi1.py
+++ day 12/Assi1.py
@@ -1,6 +1,5 @@
 import re
 from functools import lru_cache
-import copy
 
 
 pattern_dot = re.compile(r"\.+")
@@ -31,14 +30,14 @@ def calc_arragements(record: str, spring_pattern: tuple, streak: bool):
     if curr_char == "#":
         springs = re.search(pattern_spring, record)
         length = len(springs.group())
-        new_spring_pattern = list(copy.copy(spring_pattern))
+        new_spring_pattern = list(spring_pattern)
         new_spring_pattern[0] -= length
         new_spring_pattern = tuple(new_spring_pattern)
         return calc_arragements(record[length:], new_spring_pattern, True)
 
     # elif curr_char == "?":
     skip = 1
-    new_spring_pattern = list(copy.copy(spring_pattern))
+    new_spring_pattern = list(spring_pattern)
     new_spring_pattern[0] -= 1
     if len(record) > 1 and record[1] == "#":
         springs = re.search(pattern_spring, record[1:])
@@ -60,13 +59,10 @@ def main():
 
     for line in input:
         record, spring_pattern = line.split(" ")
-        record = "?".join([record] * 5)
-        spring_pattern = ",".join([spring_pattern] * 5)
         spring_pattern = [int(val) for val in spring_pattern.split(",")]
         spring_pattern = tuple(spring_pattern)
         arrangements = calc_arragements(record, spring_pattern, False)
         total_arrangements += arrangements
-        # print(f"{arrangements}: {record} {spring_pattern}")
 
     print(total_arrangements)
 
--- day 12/Assi2.py
+++ day 12/Assi2.py
@@ -1,67 +1,72 @@
-import math
-
-X = 1
-Y = 0
-EXPANDED_VALUE = 10**6
+import re
+from functools import lru_cache
 
 
-def find_expanded(universe):
-    expanded_list = []
-    for idx, line in enumerate(universe):
-        if line == ["."] * len(line):
-            expanded_list.append(idx)
-
-    return expanded_list
+pattern_dot = re.compile(r"\.+")
+pattern_spring = re.compile(r"#+")
 
 
-def calc_distance(first, second, expaded_cols, expanded_rows):
-    max_col = max(first[X], second[X])
-    min_col = min(first[X], second[X])
-    max_row = max(first[Y], second[Y])
-    min_row = min(first[Y], second[Y])
+@lru_cache(maxsize=None)
+def calc_arragements(record: str, spring_pattern: tuple, streak: bool):
+    if spring_pattern == ():
+        if record.find("#") == -1:
+            return 1
+        return 0
+    elif spring_pattern[0] == 0:
+        return calc_arragements(record[1:], spring_pattern[1:], False)
+    elif spring_pattern[0] < 0 or record == "":
+        return 0
 
-    row_delta = max_row - min_row
-    col_delta = max_col - min_col
-    total_expaded = 0
+    curr_char = record[0]
 
-    for col in expaded_cols:
-        if min_col < col and col < max_col:
-            total_expaded += 1
+    if curr_char == ".":
+        if streak:
+            return 0
 
-    for row in expanded_rows:
-        if min_row < row and row < max_row:
-            total_expaded += 1
+        dots = re.search(pattern_dot, record)
+        length = len(dots.group())
+        return calc_arragements(record[length:], spring_pattern, False)
 
-    distance = row_delta + col_delta
-    distance -= total_expaded
-    distance += total_expaded * EXPANDED_VALUE
+    if curr_char == "#":
+        springs = re.search(pattern_spring, record)
+        length = len(springs.group())
+        new_spring_pattern = list(spring_pattern)
+        new_spring_pattern[0] -= length
+        new_spring_pattern = tuple(new_spring_pattern)
+        return calc_arragements(record[length:], new_spring_pattern, True)
 
-    return distance
+    # elif curr_char == "?":
+    skip = 1
+    new_spring_pattern = list(spring_pattern)
+    new_spring_pattern[0] -= 1
+    if len(record) > 1 and record[1] == "#":
+        springs = re.search(pattern_spring, record[1:])
+        length = len(springs.group())
+        new_spring_pattern[0] -= length
+        skip += length
+    new_spring_pattern = tuple(new_spring_pattern)
+    arragements = calc_arragements(record[skip:], new_spring_pattern, True)
+    if not streak:
+        arragements += calc_arragements(record[1:], spring_pattern, False)
+    return arragements
 
 
 def main():
     with open("input.txt", "r") as file:
         input = file.read().splitlines()
 
-    universe = [list(line) for line in input]
-    expanded_rows = find_expanded(universe)
-    # Transpose
-    temp = list(map(list, zip(*universe)))
-    expaded_cols = find_expanded(temp)
+    total_arrangements = 0
 
-    galaxies_list = []
-    total_distances = 0
+    for line in input:
+        record, spring_pattern = line.split(" ")
+        record = "?".join([record] * 5)
+        spring_pattern = ",".join([spring_pattern] * 5)
+        spring_pattern = [int(val) for val in spring_pattern.split(",")]
+        spring_pattern = tuple(spring_pattern)
+        arrangements = calc_arragements(record, spring_pattern, False)
+        total_arrangements += arrangements
 
-    for row, line in enumerate(universe):
-        for col, tile in enumerate(line):
-            if tile == "#":
-                galaxies_list.append([row, col])
-
-    for ind, first in enumerate(galaxies_list):
-        for second in galaxies_list[ind + 1 :]:
-            total_distances += calc_distance(first, second, expaded_cols, expanded_rows)
-
-    print(total_distances)
+    print(total_arrangements)
 
 
 if __name__ == "__main__":
