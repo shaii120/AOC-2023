515808663a977edf91960eaa8957f92e627bd8d8
Shai Komisar <kofmisar@gmail.com>
2023-12-15 12:33:54 +0200

    day 13

---
+++ day 13/Assi2.py
@@ -0,0 +1,57 @@
+import math
+
+
+def is_mirror_rows(island, place):
+    one_mistake = False
+
+    for line in island:
+        original = line[:place]
+        mirrored = line[place : 2 * place]
+        mirrored = mirrored[::-1]
+
+        for ch, ch_m in zip(original, mirrored):
+            if ch != ch_m:
+                if one_mistake:
+                    return False
+                one_mistake = True
+    return one_mistake
+
+
+def get_mirror_val(island):
+    reverse_island = [line[::-1] for line in island]
+    mirror_range = math.ceil(len(island[0]) / 2)
+
+    island_T = list(zip(*island))
+    reverse_island_T = [line[::-1] for line in island_T]
+    mirror_range_T = math.ceil(len(island_T[0]) / 2)
+
+    for idx in range(1, mirror_range):
+        if is_mirror_rows(island, idx):
+            return idx
+        if is_mirror_rows(reverse_island, idx):
+            return len(reverse_island[0]) - idx
+
+    for idx in range(1, mirror_range_T):
+        if is_mirror_rows(island_T, idx):
+            return 100 * idx
+        if is_mirror_rows(reverse_island_T, idx):
+            return 100 * (len(reverse_island_T[0]) - idx)
+
+
+def main():
+    with open("input.txt", "r") as file:
+        input = file.read()
+        input = input.split("\n\n")
+
+    sum = 0
+
+    for island in input:
+        island = island.splitlines()
+        val = get_mirror_val(island)
+        sum += val
+
+    print(sum)
+
+
+if __name__ == "__main__":
+    main()
